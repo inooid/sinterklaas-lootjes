43150b241677f6456784a39b5680467084e8a300
Inooid <justdrawcats@gmail.com>
2014-11-25 10:09:30 +0100

    Added mailer from Gmail

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'XXX', // Enter your Gmail account here (example@gmail.com)
+        pass: 'XXX'                      // Enter your Gmail account password here
+    }
+});
 
 function shuffle(array) {
   var m = array.length, t, i;
@@ -20,16 +28,16 @@ function shuffle(array) {
 }
 
 function removeEmptyObjects(array) {
-  console.log("function:")
-  console.log(array)
+  console.log("function:");
+  console.log(array);
 
   returnArray = [];
   array.forEach(function(p, i) {
-    console.log("IN FOR EACH:")
-    console.log(returnArray)
-    if(p.name != '' || p.email != '') {
+    console.log("IN FOR EACH:");
+    console.log(returnArray);
+    if(p.name !== '' || p.email !== '') {
       // array.splice(i,1)
-      returnArray.push(p)
+      returnArray.push(p);
     }
   });
   return returnArray;
@@ -41,30 +49,50 @@ router.get('/', function(req, res) {
 });
 
 router.route('/').post(function(req, res) {
-  partsList = req.body.participants
-  console.log (partsList)
-  partsList = removeEmptyObjects(partsList)
-  console.log (partsList)
-  ticketList = partsList.slice(0) // Duplicate list
+  partsList = req.body.participants;
+  console.log (partsList);
+  partsList = removeEmptyObjects(partsList);
+  console.log (partsList);
+  ticketList = partsList.slice(0); // Duplicate list
 
-  listConnect = []
+  listConnect = [];
 
   partsList.forEach(function(participant) {
 
-    shuffle(ticketList)
-    ticket = ticketList.shift()
+    shuffle(ticketList);
+    ticket = ticketList.shift();
 
     if (participant.name === ticket.name) { // if
-      ticketList.push(ticket)
-      ticket = ticketList.shift()
+      ticketList.push(ticket);
+      ticket = ticketList.shift();
     }
 
-    listConnect.push({"participants": participant, "ticket": ticket})
-  });
+    transporter.sendMail({
+      from: {
+        name:     'JouwSinterklaasLootjes',
+        address:  'jouwsinterklaaslootjes@gmail.com'
+      },
+      to: participant.email,
+      subject: 'Hey! Je hebt een Sinterklaas lootje gekregen!',
+      text: 'Beste ' + participant.name + ', Je bent uitgenodigd voor de Sinterklaas loting. Jouw lot heeft de naam van: ' + ticket.name + '. Een fijn Sinterklaasfeest toegewenst. Groetjes, JouwSinterklaasLootjes',
+      html: '<div style="width: 100%; height: 100%; bgcolor="#cccccc"><h1>Je bent uitgenodigd voor de Sinterklaas loting!</h1><br /><p>Beste ' + participant.name + ',<br /><br />Er is voor jouw een lot gegrabbeld en daar is de volgende naam uitgekomen:</p><h2>' + ticket.name + '</h2><p>Een fijn Sinterklaasfeest toegewenst!</p><br /><p>Groetjes,<br />JouwSinterklaasLootjes</p></div>',
+    }, function(err, info) {
+      console.log(err);
+      console.log(info);
+      if (err) {
+        res.send({
+          "response": "error",
+          "message": "Helaas kon de mail niet verstuurd worden. Probeer het later nogmaals."
+        });
+      } else {
+        res.send({
+          "response": "success",
+          "message": "De lootjes zijn verdeeld en verstuurd naar de deelnemers. Veel plezier!"
+        });
+      }
+    });
 
-  res.send({
-    "response": "success",
-    "message": "Successful called the server"
+    // listConnect.push({"participants": participant, "ticket": ticket})
   });
 
 });
